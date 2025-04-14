49261d8e6f113e42440e233fa1c357454cdd5625
Tomaz Strelec <strelec.tomaz@gmail.com>
2025-04-14 20:05:02 +0200

    MS Clarity integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,13 +19,23 @@ export default function RootLayout({
           html {
             scroll-behavior: smooth;
           }
-          
           @media (prefers-reduced-motion: reduce) {
             html {
               scroll-behavior: auto;
             }
           }
         `}</style>
+        <Script
+          id="clarity-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){ (c[a].q=c[a].q||[]).push(arguments) };
+              t=l.createElement(r); t.async=1; t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "r0vuvcizjf");`
+          }}
+        />
       </head>
       <body>
         {children}
